d10b77abceb2557af50cccd54ee8a6e1083ab92c
alim <djobeyka061@gmail.com>
2023-09-27 23:02:58 +0300

    19

--- 2.3/19.py
+++ 2.3/19.py
@@ -1,1 +1,14 @@
-print(125//2)
+my_list = list(range(1, 1001))
+low = 0
+high = len(my_list) - 1
+while low <= high:
+    mid = (low + high) // 2
+    guess = my_list[mid]
+    print(guess)
+    many = str(input())
+    if many == 'Угадал!':
+        break
+    elif many == 'Меньше':
+        high = mid - 1
+    elif many == 'Больше':
+        low = mid + 1

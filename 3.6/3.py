f67ae1e878c7f621d9bacc1d512c2b2f3f822d73
alim <djobeyka061@gmail.com>
2023-11-18 11:19:36 +0300

    исправил 3 задачу

--- 3.6/3.py
+++ 3.6/3.py
@@ -1,2 +1,1 @@
-ints = [1, 2, 3, 2, 1, 0, -1, -2, -3, -2, -1, 0, 1]
-print([i for i in ints if int(str(i)[-1]) > 3] + [i for i in ints if int(str(i)[-1]) <= 3])
+[i for i in ints if int(str(i)[-1]) > 3] + [i for i in ints if int(str(i)[-1]) <= 3]
